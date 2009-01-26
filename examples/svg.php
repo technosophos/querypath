8e229e1051ebc28d182cbbea21ea568a2a3b7581
M Butcher <matt@aleph-null.tv>
2009-01-26 08:59:01 -0600

    Added a workig SVG example.

--- examples/svg.php
+++ examples/svg.php
@@ -12,4 +12,25 @@
  
 require_once '../src/QueryPath/QueryPath.php';
 
-$svg_stub = '';
+// Let's stub out a basic SVG document.
+$svg_stub = '<?xml version="1.0"?>
+<svg
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   version="1.0"
+   width="800"
+   height="600"
+   id="test">
+  <desc>Created by QueryPath.</desc>
+</svg>';
+
+qp($svg_stub)
+  ->attr(array('width' => 800, 'height' => 600))
+  ->append('<rect id="first"/><rect id="second"/>')
+  ->find('#second')
+  ->attr(array('x' => 15, 'y' => 4, 'width' => 40, 'height' => 60, 'fill' => 'red'))
+  ->prev()
+  ->attr(array('x' => 2, 'y' => 2, 'width' => 40, 'height' => 60, 'fill' => 'navy'))
+  ->writeXML();
+
