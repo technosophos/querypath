e739ac09c121bbfa338cb2654e52f1edc76ae875
M Butcher <matt@aleph-null.tv>
2009-07-02 17:38:56 -0500

    Removed docs.
    Including docs in the repository seems redundant. Docs have been
    removed.

--- docs/media/a.js
+++
@@ -1,8 +0,0 @@
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
-var pageTracker = _gat._getTracker("UA-7311468-1");
-pageTracker._trackPageview();
-} catch(err) {}
-
