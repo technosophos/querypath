9380ae1c0fcd1d2f8a471d82915016ba40596d34
M Butcher <matt@aleph-null.tv>
2009-09-02 04:53:40 -0500

    Working on Bottom-Up handler.

--- src/QueryPath/BottomUpCssEventHandler.php
+++ src/QueryPath/BottomUpCssEventHandler.php
@@ -15,13 +15,34 @@
  * @license http://opensource.org/licenses/lgpl-2.1.php LGPL (The GNU Lesser GPL) or an MIT-like license. 
  * @since 2.0
  */
- 
-class BottomUpCssEventHandler implements CssEventHandler {
+
+class SimpleSelector {
+  
+  protected $elementNS = NULL;
+  protected $element = NULL;
+  protected $elementID = NULL;
+  protected $pseudoClasses = NULL;
+  protected $pseudoElement = NULL;
+  protected $attributes = NULL;
+  protected $classes = NULL;
+  
+}
+
+/**
+ * Listen for CSS selector parsing events and collect the events.
+ *
+ * This builds an event queue that will be used as a LIFO for bottom-up searching.
+ * Calls to <code>find()</code> are passed off to a special searcher class that 
+ * performs the searching operations, beginining with the last selector and 
+ * working backward.
+ */ 
+class BottomUpCssEventCollector implements CssEventHandler {
   protected $events = array();
   protected $dom = NULL; // Always points to the top level.
   protected $matches = NULL; // The matches
   protected $alreadyMatched = array(); // Matches found before current selector.
   protected $findAnyElement = TRUE;
+  protected $handler = NULL;
   
   public function elementID($id) {
     $this->events['elementID'] = array('id' => $id);
@@ -70,9 +91,65 @@ class BottomUpCssEventHandler implements CssEventHandler {
   }
   
   /**
-   * Create a new event handler.
+   * Construct a new collector, and get a new handler, too.
    */
   public function __construct($dom) {
+    $this->handler = new BottomUpCssEventSearcher($dom, $this->getEvents());
+  }
+  
+  /**
+   * Find a pattern.
+   *
+   * This passes the finding routine off to the event handler.
+   */
+  public function find($filter) {
+    $parser = new CssParser($filter, $this);
+    $parser->parse();
+    
+    // Now we have a chain. Begin bottom-up finding.
+    $events = array_reverse($this->events, TRUE);
+    foreach ($events as $event => $params) {
+      $function = $event . 'Handler';
+      //call_user_func_array() is slow, so we pass array this way.
+      $this->handler->$function($params);
+    }
+    
+    return $this;
+  }
+  
+  public function getMatches() {
+    return $this->handler->getMatches();
+  }
+  
+  /**
+   * Get the collected events as an associative array.
+   *
+   * @return array
+   *  Associative array of event details.
+   */
+  public function getEvents() {
+    return $this->events;
+  }
+}
+
+/**
+ * This is an event handler for CSS Events.
+ *
+ * The event handler operates backward. It takes the last fired event an
+ */
+class BottomUpCssEventSearcher {
+  
+  protected $dom = NULL;
+  protected $matches = NULL;
+  protected $events = NULL;
+  
+  /**
+   * Create a new event handler.
+   */
+  public function __construct($dom, $eventQueue) {
+    
+    $this->events = $eventQueue;
+    
     // Array of DOMElements
     if (is_array($dom)) {
       $matches = array();
@@ -112,34 +189,12 @@ class BottomUpCssEventHandler implements CssEventHandler {
     }
   }
   
-  public function find($filter) {
-    $parser = new CssParser($filter, $this);
-    $parser->parse();
-    
-    // Now we have a chain. Begin bottom-up finding.
-    $events = array_reverse($this->events, TRUE);
-    foreach ($events as $event => $params) {
-      $function = $event . 'Handler';
-      //call_user_func_array() is slow, so we pass array this way.
-      $this->$function($params);
-    }
-    
-    return $this;
-  }
+  
   
   public function getMatches() {
-    return $this->matches;
+    return $this->handler->matches;
   }
-  
-  /**
-   * Get the collected events as an associative array.
-   *
-   * @return array
-   *  Associative array of event details.
-   */
-  public function getEvents() {
-    return $this->events;
-  }
+
   
   //////////////////////
   // HANDLERS //////////
