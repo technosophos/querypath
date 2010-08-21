2c8cbdb384d48ff61e5ae0182b64d3ecec222560
Emily Brand <eabrand@gmail.com>
2010-08-20 23:13:11 -0500

    Started docx, made progress on doc

---
+++ examples/docx.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * DocX Parser
+ *
+ * @package Examples
+ * @author Emily Brand
+ * @license LGPL The GNU Lesser GPL (LGPL) or an MIT-like license.
+ * @see http://www.urbandictionary.com/
+ */
+require_once '../src/QueryPath/QueryPath.php';
+
+//$path = 'http://eabrand.com/images/test.docx';
+
+$path = 'docx_document.xml';
+
+// http://www.php.net/manual/en/function.zip-open.php - For how to find the document.xml file
+
+$qp = qp($path, 'w:body');
+
+print $qp->firstChild('w:t')->text();
