8b8c7c41b039a4b407b47eee695f342e0651896e
Matt Butcher <technosophos@gmail.com>
2010-08-31 09:08:00 -0500

    Full Phar packages can now be built easily.

--- phar/basic_loader.php
+++ phar/basic_loader.php
@@ -1,7 +1,8 @@
 <?php
+// Stub for @PHAR_FILENAME@
 // Basic Phar loader. Includes QueryPath.php by default.
 try {
-    Phar::mapPhar('@PHAR_FILENAME@');
+    Phar::mapPhar('@PHAR_FILENAME@'); // Uses Phar alias.
     include 'phar://@PHAR_FILENAME@/QueryPath.php';
 } catch (PharException $e) {
     echo $e->getMessage();
