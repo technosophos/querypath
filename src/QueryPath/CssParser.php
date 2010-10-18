2b1e0adebc9044ab0b30fba2a1bf81eeac90914a
Matt Butcher <technosophos@gmail.com>
2010-10-18 14:32:14 -0500

    Fixed parser issues that were causing infinite loops.
    Under certain circumstances, the tokenizer parser could keep
    looping even though there was nothing to do. This happened when
    legal but incorrect characters where positioned in certain places.
    To fix this, I added a check in the main parsing loop to make sure
    that progress is made on each parsing pass. Why I didn't do this
    in the first place is beyond me.

--- src/QueryPath/CssParser.php
+++ src/QueryPath/CssParser.php
@@ -226,12 +226,12 @@ final class CssToken {
  * CSS.
  */
 class CssParser {
-  var $scanner = NULL;
-  var $buffer = '';
-  var $handler = NULL;
-  var $strict = FALSE;
+  protected $scanner = NULL;
+  protected $buffer = '';
+  protected $handler = NULL;
+  protected $strict = FALSE;
   
-  var $DEBUG = FALSE;
+  protected $DEBUG = FALSE;
   
   /**
    * Construct a new CSS parser object. This will attempt to
@@ -239,6 +239,7 @@ class CssParser {
    * send events to the CssEventHandler implementation.
    */
   public function __construct($string, CssEventHandler $handler) {
+    $this->originalString = $string;
     $is = new CssInputStream($string);
     $this->scanner = new CssScanner($is);
     $this->handler = $handler;
@@ -253,13 +254,29 @@ class CssParser {
    * @throws CssParseException
    */
   public function parse() {
+
     $this->scanner->nextToken();
     while ($this->scanner->token !== FALSE) {
+      // Primitive recursion detection.
+      $position = $this->scanner->position();
+      
       if ($this->DEBUG) {
         print "PARSE " . $this->scanner->token. "\n";
       }
       $this->selector();
+      
+      $finalPosition = $this->scanner->position();
+      
+      if ($this->scanner->token !== FALSE && $finalPosition == $position) {
+        // If we get here, then the scanner did not pop a single character
+        // off of the input stream during a full run of the parser, which
+        // means that the current input does not match any recognizable
+        // pattern.
+        throw new CssParseException('CSS selector is not well formed.');
+      }
+      
     }
+    
   }
   
   /**
@@ -289,7 +306,7 @@ class CssParser {
    * Handle an entire CSS selector.
    */
   private function selector() {
-    if ($this->DEBUG) print "SELECTOR\n";
+    if ($this->DEBUG) print "SELECTOR{$this->scanner->position()}\n";
     $this->consumeWhitespace(); // Remove leading whitespace
     $this->simpleSelectors();
     $this->combinator();
@@ -372,7 +389,6 @@ class CssParser {
     else {
       if ($this->DEBUG) print "COMBINATOR: no combinator found.\n";
     }
-    
   }
   
   /**
@@ -784,6 +800,13 @@ final class CssScanner {
   }
   
   /**
+   * Return the position of the reader in the string.
+   */
+  public function position() {
+    return $this->is->position;
+  }
+  
+  /**
    * See the next char without removing it from the stack.
    *
    * @return char
@@ -913,7 +936,7 @@ final class CssScanner {
         $tok = CssToken::stringLegal;
       }
       else {
-        throw new Exception('Illegal characters found in stream.');
+        throw new Exception('Illegal character found in stream: ' . $ord);
       }
     }
     
@@ -1015,7 +1038,8 @@ final class CssScanner {
  * characters.
  */
 class CssInputStream {
-  var $stream = NULL;
+  protected $stream = NULL;
+  public $position = 0;
   /**
    * Build a new CSS input stream from a string.
    * 
@@ -1041,7 +1065,11 @@ class CssInputStream {
    * stream and return it.
    */
   function consume() {
-    return array_shift($this->stream);
+    $ret = array_shift($this->stream);
+    if (!empty($ret)) {
+      $this->position++;
+    }
+    return $ret;
   }
   /**
    * Check if the stream is empty.
