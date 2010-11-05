dc3cd8b2bb01627a9a2c54a547b0c993ea87e279
Matt Butcher <technosophos@gmail.com>
2010-11-05 08:50:29 -0500

    Fixed a bug in the phar builder (Issue #43).

--- phar/basic_loader.php
+++ phar/basic_loader.php
@@ -2,8 +2,8 @@
 // Stub for @PHAR_FILENAME@
 // Basic Phar loader. Includes QueryPath.php by default.
 try {
-    Phar::mapPhar('@PHAR_FILENAME@'); // Uses Phar alias.
-    include 'phar://@PHAR_FILENAME@/QueryPath.php';
+    Phar::mapPhar('-PHAR_FILENAME%'); // Uses Phar alias.
+    include 'phar://-PHAR_FILENAME%/QueryPath.php';
 } catch (PharException $e) {
     echo $e->getMessage();
     die('Cannot initialize Phar');
