29e77c9f3a61dc8a0ed7d1801afbb1e486826d52
Matt Butcher <technosophos@gmail.com>
2012-02-23 10:01:22 -0600

    Added new base TestCase.

---
+++ test/Tests/QueryPath/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ *
+ * The master test case.
+ */
+
+namespace QueryPath\Tests;
+
+require_once 'PHPUnit/Autoload.php';
+require_once __DIR__ . '/../../../src/qp.php';
+
+class TestCase extends \PHPUnit_Framework_TestCase {
+
+}
