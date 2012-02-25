1e66046e8b7bc9fae8031c6dea35ff072c827df2
Matt Butcher <technosophos@gmail.com>
2012-02-24 21:06:31 -0600

    Made the constructor part of interface contract.

--- src/QueryPath/Query.php
+++ src/QueryPath/Query.php
@@ -1,4 +1,9 @@
 <?php
 namespace QueryPath;
 interface Query {
+  public function __construct($document, $selector, $options);
+  /*
+  public function find($selector);
+  public function count();
+   */
 }
