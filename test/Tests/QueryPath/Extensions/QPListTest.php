68d97485b6e72bdc387b403909a3175463eccb14
Matt Butcher <technosophos@gmail.com>
2012-02-27 11:12:40 -0600

    Minor namespace updates to QPList test.

--- test/Tests/QueryPath/Extensions/QPListTest.php
+++ test/Tests/QueryPath/Extensions/QPListTest.php
@@ -29,10 +29,10 @@ class QPListTests extends TestCase {
 
     // Assoc array tests...
     $list = array('a' => 'aa', 'b' => 'bb', 'c' => 'cc');
-    $qp = qp(QueryPath::HTML_STUB, 'body')->appendList($list, QPList::UL);
+    $qp = qp(\QueryPath::HTML_STUB, 'body')->appendList($list, QPList::UL);
     $this->assertEquals('aa', $qp->find(':root .qplist>li:first')->text());
 
-    $qp = qp(QueryPath::HTML_STUB, 'body')->appendList($list, QPList::DL);
+    $qp = qp(\QueryPath::HTML_STUB, 'body')->appendList($list, QPList::DL);
     $this->assertEquals('a', $qp->find(':root .qplist>dt:first')->text());
     $this->assertEquals('aa', $qp->find(':root .qplist>dd:first')->text());
     //$qp->writeXML();
@@ -48,16 +48,16 @@ class QPListTests extends TestCase {
         array('uno', 'du'), // See what happens here...
       ),
     );
-    $qp = qp(QueryPath::HTML_STUB, 'body')->appendTable($data);
+    $qp = qp(\QueryPath::HTML_STUB, 'body')->appendTable($data);
     $this->assertEquals(3, $qp->top()->find('th')->size());
     $this->assertEquals(11, $qp->top()->find('td')->size());
     $this->assertEquals('Zwei', $qp->eq(4)->text());
 
     // Test with an object instead...
-    $o = new QPTableData();
+    $o = new \QueryPath\Extension\QPTableData();
     $o->setHeaders($data['headers']);
     $o->setRows($data['rows']);
-    $qp = qp(QueryPath::HTML_STUB, 'body')->appendTable($o);
+    $qp = qp(\QueryPath::HTML_STUB, 'body')->appendTable($o);
     $this->assertEquals(3, $qp->top()->find('th')->size());
     $this->assertEquals(11, $qp->top()->find('td')->size());
     $this->assertEquals('Zwei', $qp->eq(4)->text());
