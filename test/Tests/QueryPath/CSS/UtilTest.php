e36d334e0f9550d715381b58cec5982cd0ec2c37
Matt Butcher <technosophos@gmail.com>
2012-04-23 19:35:23 -0500

    Added tests for Util.

---
+++ test/Tests/QueryPath/CSS/UtilTest.php
@@ -0,0 +1,19 @@
+<?php
+/** @file
+ * CSS Event handling tests
+ */
+namespace QueryPath\Tests;
+
+require_once __DIR__ . '/../TestCase.php';
+
+use \QueryPath\CSS\Token;
+
+/**
+ * @ingroup querypath_tests
+ * @group CSS
+ */
+class UtilTest extends TestCase {
+  public function testParseAnB() {
+
+  }
+}
