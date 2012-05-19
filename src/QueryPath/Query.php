7b9ceed1d465a3f902eb1a33d90110488190882b
Matt Butcher <technosophos@gmail.com>
2012-05-19 10:35:24 -0500

    Worked on the main interface Query.

--- src/QueryPath/Query.php
+++ src/QueryPath/Query.php
@@ -2,8 +2,11 @@
 namespace QueryPath;
 interface Query {
   public function __construct($document, $selector, $options);
-  /*
   public function find($selector);
-  public function count();
-   */
+  public function top($selector);
+  public function next($selector);
+  public function prev($selector);
+  public function siblings($selector);
+  public function parent($selector);
+  public function children($selector);
 }
